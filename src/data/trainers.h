b3685553b0720f51220a366a0736bad33cd22678
CharlesAverill <charlesaverill20@gmail.com>
2020-12-19 14:55:18 -0600

    max avery updates

--- src/data/trainers.h
+++ src/data/trainers.h
@@ -20,7 +20,7 @@ const struct Trainer gTrainers[] = {
         .items = {ITEM_FULL_RESTORE, ITEM_FULL_RESTORE, ITEM_FULL_HEAL},
         .doubleBattle = FALSE,
         .aiFlags = AI_SCRIPT_CHECK_BAD_MOVE | AI_SCRIPT_TRY_TO_FAINT | AI_SCRIPT_CHECK_VIABILITY,
-        .partySize = NELEMS(sTrainerMons_HikerMax),
+        .partySize = 6,
         .party = {.ItemCustomMoves = sTrainerMons_HikerMax}
     },
     [TRAINER_GENTLEMAN_AVERY] = {
@@ -32,7 +32,7 @@ const struct Trainer gTrainers[] = {
         .items = {ITEM_FULL_RESTORE, ITEM_FULL_RESTORE, ITEM_FULL_HEAL},
         .doubleBattle = FALSE,
         .aiFlags = AI_SCRIPT_CHECK_BAD_MOVE | AI_SCRIPT_TRY_TO_FAINT | AI_SCRIPT_CHECK_VIABILITY,
-        .partySize = NELEMS(sTrainerMons_GentlemanAvery),
+        .partySize = 6,
         .party = {.ItemCustomMoves = sTrainerMons_GentlemanAvery}
     },
     [TRAINER_TEAM_AQUA_2] = {
