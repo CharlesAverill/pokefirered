973876e2d442945555c39f73d35aa1d808af71c4
CharlesAverill <charlesaverill20@gmail.com>
2020-12-23 22:40:16 -0600

    roamer temp update

--- src/wild_pokemon_area.c
+++ src/wild_pokemon_area.c
@@ -246,8 +246,6 @@ static s32 CountRoamerNests(u16 species, struct Subsprite * subsprites)
     roamerIdx = GetRoamerIndex(species);
     if (roamerIdx < 0)
         return 0;
-    if (sRoamerPairs[roamerIdx].starter != GetStarterSpecies())
-        return 0;
     roamerLocation = GetRoamerLocationMapSectionId();
     dexAreaEntryLUTidx = 0;
     if (TryGetMapSecPokedexAreaEntry(roamerLocation, sDexAreas_Kanto, 55, &dexAreaEntryLUTidx, &dexAreaSubspriteIdx))
